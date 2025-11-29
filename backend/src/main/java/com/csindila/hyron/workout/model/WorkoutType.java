8b5f0202a61bf6bd189801c1195e95a2091b15d5
Cristian Sindila <128726794+CristianInBits@users.noreply.github.com>
2025-11-29 20:05:42 +0100

    Empezando proyecto desde cero

--- backend/src/main/java/com/csindila/hyron/workout/model/WorkoutType.java
+++
@@ -1,5 +0,0 @@
-package com.csindila.hyron.workout.model;
-
-public enum WorkoutType {
-    RUN, BIKE, SWIM, HYROX, GYM
-}
--- backend/src/main/resources/db/migration/V3__stations_add_distance.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE hyrox_station_entries
-  ADD COLUMN distance_m INTEGER CHECK (distance_m >= 0);
