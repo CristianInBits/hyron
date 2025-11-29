c3fa0dc89da29d3e99c54dbb3a0e770c5f7d3603
Cristian Sindila <128726794+CristianInBits@users.noreply.github.com>
2025-11-30 00:23:47 +0100

    BACKEND: gym files

---
+++ backend/src/main/java/dev/cristianinbits/hyron/gym/GymExerciseEntry.java
@@ -0,0 +1,55 @@
+package dev.cristianinbits.hyron.gym;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "gym_exercise_entires")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GymExerciseEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer orderIndex;
+
+    private String exerciseName;
+
+    @Column(length = 2000)
+    private String notes;
+
+    @Enumerated(EnumType.STRING)
+    private MuscleGroup muscleGroup;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "gym_workout_details_id")
+    private GymWorkoutDetails gymWorkout;
+
+    @OneToMany(mappedBy = "exerciseEntry", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<GymSet> set = new ArrayList<>();
+}
---
+++ backend/src/main/java/dev/cristianinbits/hyron/gym/MuscleGroup.java
@@ -0,0 +1,11 @@
+package dev.cristianinbits.hyron.gym;
+
+public enum MuscleGroup {
+    CHEST,
+    BACK,
+    SHOULDER,
+    LEG,
+    ARM,
+    CORE,
+    FULL_BODY
+}
