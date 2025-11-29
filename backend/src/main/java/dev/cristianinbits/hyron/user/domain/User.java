9d7309fe4281ad7265cc5b7dcd8c77a8d91e44c4
Cristian Sindila <128726794+CristianInBits@users.noreply.github.com>
2025-11-29 22:22:44 +0100

    BACKEND: creted clas User

---
+++ backend/src/main/java/dev/cristianinbits/hyron/user/domain/User.java
@@ -0,0 +1,47 @@
+package dev.cristianinbits.hyron.user.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.cristianinbits.hyron.workout.Workout;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDateTime registeredAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Workout> workouts = new ArrayList<>();
+}
