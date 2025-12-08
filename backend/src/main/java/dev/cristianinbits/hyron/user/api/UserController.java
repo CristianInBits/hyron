2a7701aa09fcce680669c870b7325d762a4111f8
CristianInBits <128726794+CristianInBits@users.noreply.github.com>
2025-12-08 02:13:47 +0100

    BACKEND: user controller creado

---
+++ backend/src/main/java/dev/cristianinbits/hyron/user/api/UserController.java
@@ -0,0 +1,56 @@
+package dev.cristianinbits.hyron.user.api;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.cristianinbits.hyron.user.dto.UserCreateRequest;
+import dev.cristianinbits.hyron.user.dto.UserResponse;
+import dev.cristianinbits.hyron.user.dto.UserUpdateRequest;
+import dev.cristianinbits.hyron.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse createUser(@Valid @RequestBody UserCreateRequest request){
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    public List<UserResponse> getAllUsers (){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponse getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public UserResponse updateUser(@PathVariable Long id, @Valid @RequestBody UserUpdateRequest request){
+        return userService.updateUser(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+    }
+}
